09b26d769ccecb5312fc08418d49063b9d139280
jgutierrezCSU <jgutierrez@mail.csuchico.edu>
2020-12-12 04:55:47 +0100

    all one arguments done, working on two arguments function

--- wordlist-editor.py
+++ wordlist-editor.py
@@ -20,18 +20,30 @@ def single_arg(filename, num_length):
         if os.path.isfile("b_"+filename): #check if file is there , if true , delete (clear)
             os.remove("b_"+filename)
         newfile = open( "b_"+filename, 'a')
+    elif args.firstupper:
+        if os.path.isfile("fU_"+filename): #check if file is there , if true , delete (clear)
+            os.remove("fU_"+filename)
+        newfile = open( "fU_"+filename, 'a')
+    elif args.allupper:
+        if os.path.isfile("aU_"+filename): #check if file is there , if true , delete (clear)
+            os.remove("aU_"+filename)
+        newfile = open( "aU_"+filename, 'a')
+    elif args.alllower:
+        if os.path.isfile("aL_"+filename): #check if file is there , if true , delete (clear)
+            os.remove("aL_"+filename)
+        newfile = open( "aL_"+filename, 'a')
+    elif args.firstlower:
+        if os.path.isfile("fL_"+filename): #check if file is there , if true , delete (clear)
+            os.remove("fL_"+filename)
+        newfile = open( "fL_"+filename, 'a')
     
     for line in text:
         words=clean_lines(line) # parse " " & Spaces
         for word in words:
             print(word)
             counter=0
-            if args.end and args.beg:
-                while counter != 10**num_length:
-                    # concat and append nums to word
-                     newfile.write(str(counter) + word + str(counter)+'\n')
-                     counter += 1
-            elif args.end:
+
+            if args.end:
                 while counter != 10**num_length:
                     # concat and append nums to word
                     newfile.write(word + str(counter) + '\n')
@@ -41,6 +53,17 @@ def single_arg(filename, num_length):
                     # concat and append nums to word
                      newfile.write(str(counter) + word + '\n')
                      counter += 1        
+            elif args.firstupper:
+                newfile.write(word[0].upper() + word[1:]+ '\n')
+
+            elif args.allupper:
+                newfile.write(word.upper() + '\n')
+
+            elif args.firstlower:
+                newfile.write(word[0].lower() + word[1:]+ '\n')
+
+            elif args.alllower:
+                newfile.write(word.lower() + '\n')
 
     print('saved ...')
 
@@ -53,6 +76,11 @@ def two_arg(filename, num_length):
             os.remove("eb_"+filename)
         newfile = open( "eb_"+filename, 'a')
 
+    if args.end and args.firstupper:
+        if os.path.isfile("efU_"+filename): #check if file is there , if true , delete (clear)
+            os.remove("efU_"+filename)
+        newfile = open( "efU_"+filename, 'a')
+
     for line in text:
         words=clean_lines(line) # parse " " & Spaces
         for word in words:
@@ -62,7 +90,12 @@ def two_arg(filename, num_length):
                 while counter != 10**num_length:
                     # concat and append nums to word
                      newfile.write(str(counter) + word + str(counter)+'\n')
-                     counter += 1    
+                     counter += 1 
+            elif args.end and args.firstupper:
+                while counter != 10**num_length:
+                    # concat and append nums to word
+                    newfile.write(word[0].upper() + word[1:]+ str(counter) + '\n')
+                    counter += 1
 
     print('saved ...')
 
@@ -80,10 +113,10 @@ parser.add_argument("--end",type =int,help='adds digits (0-9) at end of each wor
 parser.add_argument("--beg",type =int ,help='adds digits (0-9) at beginning of each word.Pass in Num of digits') 
 
 #exclusive
-group.add_argument("--firstupper",action="store_true") #no argument needed w/ action="store_true"  
-group.add_argument("--allupper",action="store_true" )  
-group.add_argument("--firstlower",action="store_true")
-group.add_argument("--alllower",action="store_true")  
+group.add_argument("--firstupper",action="store_true",help='first letter to upper') #no argument needed w/ action="store_true"  
+group.add_argument("--allupper",action="store_true" ,help='all letters to upper')  
+group.add_argument("--firstlower",action="store_true",help='first letter to lower')
+group.add_argument("--alllower",action="store_true",help='all letter to lower')  
 
 #bind the arguments
 args = parser.parse_args();
@@ -97,14 +130,9 @@ if argument_length == 3 or argument_length == 4:
         single_arg(args.filename,args.end)
     elif args.beg:  # append at beg 
         single_arg(args.filename,args.beg)
-    elif args.firstupper:
-        print("firstupper")
-    elif args.allupper:
-        print("allupper")
-    elif args.firstlower:
-        print("firstlower")
-    elif args.alllower:
-        print("alllower")
+    
+    single_arg(args.filename,-1)
+    
 
 # two arguments
 elif argument_length == 5 or argument_length == 6:
@@ -114,7 +142,7 @@ elif argument_length == 5 or argument_length == 6:
             exit()
         two_arg(args.filename,args.beg)
     elif args.end and args.firstupper:
-        print("end firstupper")
+        two_arg(args.filename,args.end)
     elif args.end and args.allupper:
         print("end allupper")
     elif args.end and args.firstlower:
