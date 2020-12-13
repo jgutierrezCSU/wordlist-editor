55e296ffe590fc10357afe34c2b36910d54ba158
jgutierrezCSU <jgutierrez@mail.csuchico.edu>
2020-12-13 02:52:45 +0100

    manual additon function complete

--- wordlist-editor.py
+++ wordlist-editor.py
@@ -177,13 +177,14 @@ if len(sys.argv) == 3:
 #for argparser
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
+group2 = parser.add_mutually_exclusive_group()
 parser.add_argument("filename",help='File to edit') #required argument
 parser.add_argument("outfilename",help='Name of your outfile') #required argument
-parser.add_argument("--end",type =int,help='adds digits (0-9) at end of each word.Pass in Num of digits' ) 
-parser.add_argument("--beg",type =int ,help='adds digits (0-9) at beginning of each word.Pass in Num of digits') 
-# need to work on this
-parser.add_argument("--manfin",help='manually add input to end of each word') 
-parser.add_argument("--manbeg",help='manually add input to beginning of each word') 
+group2.add_argument("--end",type =int,help='adds digits (0-9) at end of each word.Pass in Num of digits' ) 
+group2.add_argument("--beg",type =int ,help='adds digits (0-9) at beginning of each word.Pass in Num of digits') 
+
+group2.add_argument("--manfin",help='manually add input to end of each word') 
+group2.add_argument("--manbeg",help='manually add input to beginning of each word') 
 
 
 #exclusive
